8270f71072fb88493a377eb8b8c2aae5fb8ffa23
Rokatacaem <rokataca@gmail.com>
2026-05-02 21:27:00 -0400

    fix: eliminar ruta duplicada para corregir build

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootPage() {
-    return (
-        <div style={{ background: 'black', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <h1>Fechillar Root</h1>
-        </div>
-    );
-}
